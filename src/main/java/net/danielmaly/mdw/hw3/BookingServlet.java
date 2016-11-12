06d067f4e775d8a4f40a39de728f7ba6de1f3c53
Daniel Maly <maly.dan@gmail.com>
2016-11-12 21:40:44 +0100

    Finished

--- src/main/java/net/danielmaly/mdw/hw3/BookingServlet.java
+++ src/main/java/net/danielmaly/mdw/hw3/BookingServlet.java
@@ -11,7 +11,42 @@ public class BookingServlet extends HttpServlet {
     public void doGet (HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
         PrintWriter out = res.getWriter();
 
-        out.println("Hello, world!");
+        String newState = req.getParameter("state");
+
+        if (newState != null && newState.equals("new")) {
+            HttpSession session = req.getSession();
+            String bookingData = req.getParameter("data");
+            if(bookingData != null) {
+                session.setAttribute("bookingData", bookingData);
+                session.setAttribute("bookingState", newState);
+                out.println("Created booking " + bookingData + " with state NEW.");
+            }
+            else {
+                out.println("Missing parameter bookingData");
+            }
+        }
+        else if(newState != null && (newState.equals("payment")|| newState.equals("completed"))) {
+            HttpSession session = req.getSession(false);
+            if(session == null) {
+                out.println("No existing session for this booking.");
+            }
+            else {
+                String currentState = (String) session.getAttribute("bookingState");
+                String bookingData = (String) session.getAttribute("bookingData");
+                if(currentState.equals("payment") && newState.equals("completed")) {
+                    session.invalidate();
+                    out.println("Booking " + bookingData  + " is completed. Terminating session. Goodbye!");
+                }
+                else if(currentState.equals("new") && newState.equals("payment")) {
+                    session.setAttribute("bookingState", newState);
+                    out.println("Booking " + bookingData + " has been transitioned to state PAYMENT.");
+                }
+            }
+        }
+        else {
+            out.println("Invalid or missing state parameter. Request has been ignored.");
+        }
+
         out.close();
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2>Navigate to /booking to start</h2>
 </body>
 </html>
